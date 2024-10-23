b6cc530ef4b30ed6a97111fe4e126b5980a0fe7d
Polina <polina_cherkasova@outlook.com>
2024-10-23 21:31:08 +0300

    added middleware & e

---
+++ src/middlewares/ValidationMiddleware.ts
@@ -0,0 +1,27 @@
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { Request, Response, NextFunction } from 'express';
+
+export function validationMiddleware(type: any): (request: Request, response: Response, next: NextFunction) => void {
+    return (
+        request: Request,
+        response: Response,
+        next: NextFunction
+    ): void => {
+        const dtoObject = plainToClass(type, request.body);
+
+        validate(dtoObject).then((errors) => {
+            if (errors.length > 0) {
+                return response.status(400).json({
+                    message: 'Validation failed',
+                    errors: errors.map(err => ({
+                        property: err.property,
+                        constraints: err.constraints
+                    })),
+                });
+            } else {
+                next();
+            }
+        });
+    };
+}
